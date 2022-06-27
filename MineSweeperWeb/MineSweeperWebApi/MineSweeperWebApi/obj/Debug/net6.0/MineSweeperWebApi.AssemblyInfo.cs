31198812aec3d93f74fe1a86312a76dc88336dca
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-06-27 11:56:23 +0300

    updated

--- MineSweeperWeb/MineSweeperWebApi/MineSweeperWebApi/obj/Debug/net6.0/MineSweeperWebApi.AssemblyInfo.cs
+++ MineSweeperWeb/MineSweeperWebApi/MineSweeperWebApi/obj/Debug/net6.0/MineSweeperWebApi.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("9a2f871d-62bf-42e5-9890-47dc40c29565")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("MineSweeperWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
